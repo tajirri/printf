61b6e7b80eaa78ec82c7745270e1f6b745585999
tajirri <clarencewagah@gmail.com>
2023-11-14 08:21:22 -0800

    task 0 second commit

--- main.h
+++ main.h
@@ -5,5 +5,8 @@ int printf_str(va_list vl);
 int printf_ch(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _strlen(char *str);
+int _strlench(const char *str);
+int printf_37(void);
 
 #endif
---
+++ printlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ * _strlench - _strlen function on char
+ * @str: string pointer
+ * Return: 1
+ */
+
+int _strlen(char *str)
+{
+	int n;
+	for (n = 0; str[n] != 0; n++)
+	return (n);
+}
+
+int _strlench(const char *str)
+{
+	int n;
+	for (n = 0; str[n] != 0; n++);
+	retuen (n);
+}
+
