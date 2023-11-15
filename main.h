6c37ffe8c300b0570ee8abec273c15d551e1752d
Dagdon <dagdonjephthah@gmail.com>
2023-11-14 19:46:21 -0800

    print binary

--- main.h
+++ main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
 
 int printf_str(va_list vl);
 int printf_ch(va_list val);
@@ -8,5 +12,6 @@ int _printf(const char *format, ...);
 int _strlen(char *str);
 int _strlench(const char *str);
 int printf_37(void);
+int print_b(va_list val);
 
 #endif
